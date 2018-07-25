ef6f2ce955dd3c18dd0fe752f750a13e1fc20ebb
Zhanpeng Wang <zwang10@haverford.edu>
2018-07-25 15:47:43 -0400

    Updating the website in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,10 @@ var windowScroll = function () {
         var scrollPos = $(this).scrollTop();
         
         var system ={win : false,mac : false,xll : false};
-        //检测平台
         var p = navigator.platform;
         system.win = p.indexOf("Win") == 0;
         system.mac = p.indexOf("Mac") == 0;
         system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
-        //判断平台类型
         if(system.win||system.mac||system.xll){
             if ($(window).scrollTop() > 70)
             {
@@ -19,7 +17,6 @@ var windowScroll = function () {
                 $('.site-header').removeClass('site-header-nav-scrolled');
             }
         }else{
-            //如果是手机则将顶栏移除界面
             if ($(window).scrollTop() > 40) 
             {
                 $('.site-header').addClass('site-header-nav-scrolled-ph');
